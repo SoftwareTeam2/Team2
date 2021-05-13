b62a6d9692497a517687ebda308adc0424e93dc0
KimTaeKang57 <83891837+KimTaeKang57@users.noreply.github.com>
2021-05-13 13:55:20 +0900

    Update Customer.java

--- src/booksys/application/domain/Customer.java
+++ src/booksys/application/domain/Customer.java
@@ -6,17 +6,27 @@
  * McGraw-Hill (2004)
  */
 
+/*
+ * Restaurant Booking System: example code to accompany
+ *
+ * "Practical Object-oriented Design with UML"
+ * Mark Priestley
+ * McGraw-Hill (2004)
+ */
+
 package booksys.application.domain ;
 
 public class Customer
 {
   private String name ;
   private String phoneNumber ;
-
-  public Customer(String n, String p)
+  private boolean IsCar;
+  
+  public Customer(String n, String p, boolean Ic)
   {
     name = n ;
     phoneNumber = p ;
+    IsCar = Ic;
   }
 
   public String getName()
@@ -27,5 +37,9 @@ public class Customer
   public String getPhoneNumber()
   {
     return phoneNumber ;
+  }
+  
+  public boolean getIsCar() {
+	  return IsCar;
   }
 }
