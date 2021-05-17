d274e597b42619d25ac14eaecca69b179313827f
KimTaeKang57 <83891837+KimTaeKang57@users.noreply.github.com>
2021-05-17 11:32:36 +0900

    Add files via upload

---
+++ letsgo-spring/letsgo-spring/src/main/java/letsgo/letsgospring/SpringConfig.java
@@ -0,0 +1,28 @@
+package letsgo.letsgospring;
+
+import letsgo.letsgospring.repository.*;
+import letsgo.letsgospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+   private EntityManager em;
+
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
+    @Bean public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+// return new MemoryMemberRepository();
+        return new JpaMemberRepository(em);
+    }
+}
---
+++ letsgo-spring/letsgo-spring/src/main/java/letsgo/letsgospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package letsgo.letsgospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
